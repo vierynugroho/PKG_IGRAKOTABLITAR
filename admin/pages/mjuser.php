6c13d5f907f62468a904c9798a97752908077371
Viery Nugroho <viery15102002@gmail.com>
2024-01-30 23:55:00 +0700

    fix UI jenis user

--- admin/pages/mjuser.php
+++ admin/pages/mjuser.php
@@ -74,11 +74,11 @@
                         <div class="accordion-item">
                             <h2 class="accordion-header"
                                 id="headingOne">
-                                <button class="accordion-button"
+                                <button class="accordion-button collapsed"
                                         type="button"
                                         data-bs-toggle="collapse"
                                         data-bs-target="#collapsePenilaian"
-                                        aria-expanded="true"
+                                        aria-expanded="false"
                                         aria-controls="collapsePenilaian">
                                     Keterangan Role User
                                 </button>
